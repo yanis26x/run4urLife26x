5327bc8cea1dc409be563dacad735e87c78a762a
yanis26x <yanis.djenadi@hotmail.com>
2025-09-16 16:23:38 -0400

    ajout porte de fin de niv

--- Assets/ScriptProf/DoorOpener.cs
+++ Assets/ScriptProf/DoorOpener.cs
@@ -1,29 +1,33 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
  
 public class DoorOpener : MonoBehaviour
 {
-    public GameObject door;      // Assigner la porte dans l’Inspector
-    private Animator doorAnimator;
+
+    public string sceneName;
+    //public GameObject door;      // Assigner la porte dans l’Inspector
+    // private Animator doorAnimator;
  
-    void Start()
-    {
-        doorAnimator = door.GetComponent<Animator>();
-    }
+    // void Start()
+    // {
+    //     doorAnimator = door.GetComponent<Animator>();
+    // }
  
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            doorAnimator.SetTrigger("OpenDoor");
+            // doorAnimator.SetTrigger("OpenDoor");
+            SceneManager.LoadScene(sceneName);
         }
     }
  
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            doorAnimator.SetTrigger("CloseDoor");
-        }
-    }
+    // private void OnTriggerExit2D(Collider2D other)
+    // {
+    //     if (other.CompareTag("Player"))
+    //     {
+    //         doorAnimator.SetTrigger("CloseDoor");
+    //     }
+    // }
     
 }
