f7d5f9cdc5c4e55a69357ec347e8d7de7016ec00
yanis26x <yanis.djenadi@hotmail.com>
2025-09-23 13:05:16 -0400

    clean code

--- Assets/SCRIPT/Menu.cs
+++ Assets/SCRIPT/Menu.cs
@@ -6,7 +6,7 @@ public class Menu : MonoBehaviour
     public void StartGame()
     {
         
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("Lvl1");
     }
 
     
